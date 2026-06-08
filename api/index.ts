c2dcc782afb999c02fe0eae3f3a130ee4c20fecb
Santhosh <sadaiyappan.s@numentica-ui.com>
2026-06-08 10:32:06 +0530

    fixed api

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../backend/src/app.module';
+import express from 'express';
+import { Request, Response } from 'express';
+
+const server = express();
+
+let app: any;
+
+async function bootstrap() {
+  if (!app) {
+    app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(server),
+    );
+    app.enableCors();
+    await app.init();
+  }
+  return server;
+}
+
+export default async function handler(req: Request, res: Response) {
+  await bootstrap();
+  server(req, res);
+}
