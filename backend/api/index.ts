2dc189319b9a1c8c82d9b03d7768392066693d59
Santhosh <sadaiyappan.s@numentica-ui.com>
2026-06-08 12:38:13 +0530

    fixed

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
-import { AppModule } from '../src/app.module';
+import { AppModule } from '../dist/app.module';
 
 const server = express();
 
--- backend/dist/api/index.js
+++ backend/dist/api/index.js
@@ -7,7 +7,7 @@ exports.default = handler;
 const core_1 = require("@nestjs/core");
 const platform_express_1 = require("@nestjs/platform-express");
 const express_1 = __importDefault(require("express"));
-const app_module_1 = require("../src/app.module");
+const app_module_1 = require("../dist/app.module");
 const server = (0, express_1.default)();
 let isInitialized = false;
 async function bootstrap() {
